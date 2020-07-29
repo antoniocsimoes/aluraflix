18469b54ab8f9814508d2f1f53c8eb54c622ca8c
antoniocsimoes <antoniocssimoes@gmail.com>
2020-07-28 21:37:06 -0300

    aula2 imersão alura - criação páginas video e categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 // import ButtonLink from './components/ButtonLink';
@@ -8,13 +9,13 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/"> 
+            <Link to="/"> 
                 <img className="Logo" src={Logo} alt="AluraFlix Logo" />
-            </a>
-            {/* <ButtonLink className="ButtonLink" href="/">
+            </Link>
+            {/* <ButtonLink className="ButtonLink" to="/">
                 Novo vídeo
             </ButtonLink> */}
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+//teste
+
+
+const Pagina404 = () => (<div>Página 404!</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />   
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />        
+      <Route component={Pagina404} />
+    </Switch>
+   
+  </BrowserRouter>,
+
+
+
+  // <React.StrictMode>S
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
