536618d991150cbe3264f0f1a89372ca6e996afb
antoniocsimoes <antoniocssimoes@gmail.com>
2020-07-28 22:27:22 -0300

    Início formulário categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,18 @@ function CadastroCategoria() {
     return (
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
+        <form>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
 
         <Link to="/">
             Ir para home
